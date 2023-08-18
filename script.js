ae3914dbabe08369c0a2406cbd958640408a8c81
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-18 10:34:18 +0530

    update_js-file

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 let choices = document.querySelectorAll(".choices button");
 let resultElement = document.getElementById("result");
 
+
 let playerChoice;
 
 function getPlayerChoice(choice) {
+
   playerChoice = choice;
+
   playGame();
+
 }
 
 function playGame() {
+
   let computerChoice = getComputerChoice();
+
   let result = getResult(playerChoice, computerChoice);
+
   displayResult(playerChoice, computerChoice, result);
+
 }
 
 function getComputerChoice() {
+
   let choices = ["rock", "paper", "scissors"];
+
   let randomIndex = Math.floor(Math.random() * 3);
+
   return choices[randomIndex];
 }
 
 function getResult(player, computer) {
+  
   if (player === computer) {
     return "It's a tie!";
   } else if (
